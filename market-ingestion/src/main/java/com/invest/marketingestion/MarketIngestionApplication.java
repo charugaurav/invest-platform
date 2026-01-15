4aeaf6f08dddb4a647b47a063ef657ca3ec3b6db
charugaurav <gg.charu@gmail.com>
2026-01-15 14:39:23 +0530

    Build setup

---
+++ market-ingestion/src/main/java/com/invest/marketingestion/MarketIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.invest.marketingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketIngestionApplication.class, args);
+	}
+
+}
---
+++ market-ingestion/src/test/java/com/invest/marketingestion/MarketIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invest.marketingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketIngestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
